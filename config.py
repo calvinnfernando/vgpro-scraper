189821e7e6bb50673379a9672039227c7f8af466
Calvin Fernando Tjoeng <calvin.fernando@yahoo.com>
2018-09-30 23:12:44 -0700

    slight modification to .gitignore

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 # SPECIFIC_HERO_NAMES (array of strings)
 
 CONFIG = {
-	'PLAYER_USERNAME_STR' : 'calvinfernando',
+	'PLAYER_USERNAME_STR' : 'USERNAME',
 	'DISPLAY_X_LAST_GAMES' : 10 * 2,				# MUST BE MULTIPLE OF 10, VALUE SHOULD BE GREATER THAN 10.
-	'SPECIFIC_HERO_NAMES' : ['anka', 'lyra', 'baron', 'vox', 'phinn']
+	'SPECIFIC_HERO_NAMES' : ['HERO_1', 'HERO_2', 'HERO_3', 'HERO_4', 'HERO_5']
 }
