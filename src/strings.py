151c05752c32c8b6b213126bc95bc576d9df9507
Calvin Fernando Tjoeng <calvin.fernando@yahoo.com>
2018-10-03 15:51:23 -0700

    applied CRA to current directory and applied headless chrome instead of phantomjs (deprecated)

---
+++ src/strings.py
@@ -0,0 +1,60 @@
+# Useful strings for vgproSorter.py
+# Do not make any changes to this file!
+
+# VGPRO.GG WEB URL
+WEB_URL = 'https://vgpro.gg/players/'
+
+# STYLE BACKGROUND IMAGE URL
+BG_IMAGE_URL_STR = 'background-image: url("https://vgproassets.nyc3.cdn.digitaloceanspaces.com/'
+PNG_STR = '.png");'
+HEROES_STR = 'heroes/'
+ITEMS_STR = 'items/'
+
+# BORDER & INDENT
+BORDER_TOP_BOT = '**************************************************************************'
+BORDER_HEROES = '   -------------------------------------------------------------------'
+INDENT_STR = '    '
+DOUBLE_INDENT_STR = '        '
+
+# SELENIUM
+LOAD_SELENIUM_STR = '[Selenium] Loading...'
+DONE_SELENIUM_STR = '[Selenium] Done!'
+
+# BEAUTIFULSOUP
+LOAD_BS_STR = '[BeatifulSoup] Loading...'
+DONE_BS_STR = '[BeatifulSoup] Done!'
+
+# PANDAS
+LOAD_PANDAS_STR = '[pandas] Loading...'
+DONE_PANDAS_STR = '[pandas] Done!'
+
+# RESULTS
+RESULT_HEADER = 'Showing the results within your last '
+
+# VAINGLORY MODES (array)
+MODES_ARR = ['Ranked 3v3', 'Ranked 5v5', 'Casual 3v3', 'Casual 5v5', 'Blitz', 'Battle Royale']
+
+# VAINGLORY HEROES - alphabetically ordered
+HEROES_ARR = ['adagio', 'alpha', 'anka', 'ardan', 'baptiste', 'baron', 'blackfeather', 'catherine',
+			  'celeste', 'churnwalker', 'flicker', 'fortress', 'glaive', 'grace', 'grumpjaw', 'gwen',
+			  'idris', 'joule', 'kensei', 'kestrel', 'kinetic', 'koshka', 'krul', 'lance', 'lorelai',
+			  'lyra', 'malene', 'ozo', 'petal', 'phinn', 'reim', 'reza', 'ringo', 'rona', 'samuel',
+			  'saw', 'silvernail', 'skaarf', 'skye', 'taka', 'tony', 'varya', 'vox', 'yates']
+
+# VAINGLORY ITEMS - alphabetically ordered
+ITEMS_ARR = ['aegis', 'aftershock', 'alternating-current', 'atlas-pauldron', 'barbed-needle',
+			 'blazing-salvo', 'bonesaw', 'book-of-eulogies', 'breaking-point', 'broken-myth',
+			 'capacitor-plate', 'chronograph', 'clockwork', 'coat-of-plates', 'contraption',
+			 'crucible', 'crystal-bit', 'crystal-infusion', 'dragons-eye', 'dragonblood-contract',
+			 'dragonheart', 'eclipse-prism', 'energy-battery', 'eve-of-harvest', 'flare',
+			 'flare-gun', 'flare-loader', 'fountain-of-renewal', 'frostburn', 'halcyon-chargers',
+			 'heavy-prism', 'heavy-steel', 'hourglass', 'ironguard-contract', 'journey-boots',
+			 'kinetic-shield', 'lifespring', 'light-armor', 'light-shield', 'lucky-strike',
+			 'metal-jacket', 'minion-candy', 'minions-foot', 'nullwave-gauntlet', 'oakheart',
+			 'piercing-shard', 'piercing-spear', 'poisoned-shiv', 'protector-contract',
+			 'pulseweave', 'reflex-block', 'rooks-decree', 'scout-trap', 'scoutpak', 'scouttuff',
+			 'serpent-mask', 'shatterglass', 'shiversteel', 'six-sins', 'slumbering-husk',
+			 'sorrowblade', 'spellfire', 'spellsword', 'sprint-boots', 'stormcrown',
+			 'stormguard-banner', 'superscout-2000', 'swift-shooter', 'teleport-boots',
+			 'tension-bow', 'tornado-trigger', 'travel-boots', 'tyrants-monocle', 'void-battery',
+			 'war-treads', 'weapon-blade', 'weapon-infusion']
