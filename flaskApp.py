4d7ae676335d231fbec28ec873a9cfb2f4af7d4c
Calvin Fernando Tjoeng <calvin.fernando@yahoo.com>
2018-10-09 19:19:46 -0700

    moved python files from ./src/ to . and working on extra features in react (i.e. remove button and showing result)

---
+++ flaskApp.py
@@ -0,0 +1,31 @@
+# import libraries
+from flask import Flask, request
+from sortHeroes import sortHeroes
+import config
+
+# create app
+app = Flask(__name__)
+
+# route sortHeroes
+@app.route('/sortFunc', methods = ['GET'])
+def sortFunc():
+	if request.method == 'GET':
+		playerName = request.args.get('name', None)
+		if not playerName:
+			return "Player name not provided."
+		config.CONFIG['PLAYER_USERNAME_STR'] = playerName
+
+		numGames = request.args.get('numGames', None)
+		if not numGames:
+			return "Number of games not provided."
+		config.CONFIG['DISPLAY_X_LAST_GAMES'] = int(numGames)
+
+		heroSelected = request.args.getlist('hero', None)
+		if not heroSelected:
+			return "No heroes provided."
+		config.CONFIG['SPECIFIC_HERO_NAMES'] = heroSelected
+
+		return sortHeroes()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ src/result.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+class result extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			error: null,
+			isLoaded: false,
+			heroNamesArr: [],
+			avgKDAArr: [],
+			killArr: [],
+			deathArr: [],
+			assistArr: [],
+			kdaArr: [],
+			winLossArr: [],
+			modesArr: [],
+			itemsArr: []
+		};
+	}
+
+	componentDidMount(){
+		fetch("http://localhost:5000/sortFunc")
+			.then(res => res.json())
+			.then(
+				(result) => {
+					this.setState({
+						isLoaded: true,
+						heroNamesArr: result.hero_names_arr,
+						avgKDAArr: result.avg_kda_arr,
+						killArr: result.kills_arr,
+						deathArr: result.deaths_arr,
+						assistArr: result.assist_arr,
+						kdaArr: result.kda_arr,
+						winLossArr: result.win_loss_arr,
+						modesArr: result.modes_arr,
+						itemsArr: result.items_arr
+					});
+				},
+				(error) => {
+					this.setState({
+						isLoaded: true,
+						error
+					});
+				}
+			)
+	}
+
+	render(){
+		const {error, isLoaded, heroNamesArr, avgKDAArr, killArr, deathArr, assistArr, kdaArr, winLossArr, modesArr, itemsArr} = this.state;
+		if (error) {
+			return <div>Error: {error.message}</div>;
+		} else if (!isLoaded) {
+			return <div>Loading... This may take some time.</div>;
+		} else {
+			return (
+				<div className="sortResult">
+					<ul>
+						{heroNamesArr.map(heroName => (
+							<li key={heroName}>{heroName}</li>
+						))}
+					</ul>
+				</div>
+			);
+		}
+	}
+}
